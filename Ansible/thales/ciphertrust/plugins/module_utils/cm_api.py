df9a1548f5e05d61b76fb3f405e179e7dd4d0b89
anugram <anurag.jain@thalesgroup.com>
2023-01-29 13:58:57 -0500

    Indent error

--- Ansible/thales/ciphertrust/plugins/module_utils/cm_api.py
+++ Ansible/thales/ciphertrust/plugins/module_utils/cm_api.py
@@ -94,7 +94,7 @@ def POSTData(payload=None, cm_node=None, cm_api_endpoint=None, id=None):
         verify=False)
 
       response = _data.json()
-      if response.status_code == "201":
+      if _data.status_code == "201":
         __ret = {
           "id": response[id],
           "created_at": response["created_at"],

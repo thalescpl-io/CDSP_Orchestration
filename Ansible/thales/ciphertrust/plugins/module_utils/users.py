832450a49e42eb9c6eddc3497847635c83288d18
anugram <anurag.jain@thalesgroup.com>
2023-01-29 12:33:51 -0500

    Update users.py

--- Ansible/thales/ciphertrust/plugins/module_utils/users.py
+++ Ansible/thales/ciphertrust/plugins/module_utils/users.py
@@ -52,28 +52,28 @@ def create(**kwargs):
           )
       return response
 
-      if response["status_code"] == "201":
-        __ret = {
-          "user_id": response["data"]["user_id"],
-          "created_at": response["data"]["created_at"],
-          "message": "User created sucessfully"
-        }
-        return __ret
-      else:
-        if is_json(str(response["data"])):
-          if "codeDesc" in response["data"].json():
-            codeDesc=response["data"].json()["codeDesc"]
-            __ret = {
-              "message": "User creation failed",
-              "err": codeDesc
-            }
-            return __ret
-        else:
-            __ret = {
-              "message": "User creation failed",
-              "err": str(response["data"])
-            }
-            return __ret
+    #   if response["status_code"] == "201":
+    #     __ret = {
+    #       "user_id": response["data"]["user_id"],
+    #       "created_at": response["data"]["created_at"],
+    #       "message": "User created sucessfully"
+    #     }
+    #     return __ret
+    #   else:
+    #     if is_json(str(response["data"])):
+    #       if "codeDesc" in response["data"].json():
+    #         codeDesc=response["data"].json()["codeDesc"]
+    #         __ret = {
+    #           "message": "User creation failed",
+    #           "err": codeDesc
+    #         }
+    #         return __ret
+    #     else:
+    #         __ret = {
+    #           "message": "User creation failed",
+    #           "err": str(response["data"])
+    #         }
+    #         return __ret
     except:
       raise
 

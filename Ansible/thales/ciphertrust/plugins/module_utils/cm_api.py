afcd6e5ace28e15e0fe4f01bd82f278bc0e9ba42
anugram <anurag.jain@thalesgroup.com>
2023-02-06 08:17:14 -0500

    GETData debugging

--- Ansible/thales/ciphertrust/plugins/module_utils/cm_api.py
+++ Ansible/thales/ciphertrust/plugins/module_utils/cm_api.py
@@ -361,34 +361,47 @@ def DeleteWithoutData(cm_node=None, cm_api_endpoint=None):
         return jsonErr
 
 def GETData(cm_node=None, cm_api_endpoint=None):
-    node = ast.literal_eval(cm_node)
-    # Create the session object
-    cmSessionObject = CMAPIObject(
-        cm_api_user=node["user"],
-        cm_api_pwd=node["password"],
-        cm_url=node ["server_ip"],
-        cm_api_endpoint=cm_api_endpoint,
-        verify=False,
-    )
-    try:
-        response = requests.get(cmSessionObject["url"], headers=cmSessionObject["headers"], verify=False)
-        if is_json(str(response)): 
-          if "codeDesc" in response.json():
-            raise CMApiException(message="Error fetching data: " + response.json()["codeDesc"], api_error_code=response.status_code)
-          else:
-            return response
-        else:
-            raise CMApiException(message="Error fetching data", api_error_code=response.status_code)
-            
-    except requests.exceptions.HTTPError as errh:
-      raise AnsibleCMException(message="HTTPError: cm_api >> " + errh)
-    except requests.exceptions.ConnectionError as errc:
-      raise AnsibleCMException(message="ConnectionError: cm_api >> " + errc)
-    except requests.exceptions.Timeout as errt:
-      raise AnsibleCMException(message="TimeoutError: cm_api >> " + errt)
-    except requests.exceptions.RequestException as err:
-      raise AnsibleCMException(message="ErrorPath: cm_api >> " + err)
 
+  # Create the session object
+  node = ast.literal_eval(cm_node)
+  pattern_2xx = re.compile(r'20[0-9]')
+  pattern_4xx = re.compile(r'40[0-9]')
+  cmSessionObject = CMAPIObject(
+    cm_api_user=node["user"],
+    cm_api_pwd=node["password"],
+    cm_url=node["server_ip"],
+    cm_api_endpoint=cm_api_endpoint,
+    verify=False,
+  )
+
+  try:
+    _data = requests.get(
+      cmSessionObject["url"], 
+      headers=cmSessionObject["headers"], 
+      verify=False)
+
+    response = _data.json()
+    
+    if response["resources"] == None:
+      raise CMApiException(message="Error fetching data " + str(response), api_error_code=_data.status_code)
+
+    if len(response["resources"]) > 0:
+      __ret = {
+        "id": response["resources"][0][id]
+      }
+    else:
+      raise CMApiException(message="No recors found", api_error_code=_data.status_code)
+
+    return __ret
+  except requests.exceptions.HTTPError as errh:
+    raise AnsibleCMException(message="HTTPError: cm_api >> " + errh)
+  except requests.exceptions.ConnectionError as errc:
+    raise AnsibleCMException(message="ConnectionError: cm_api >> " + errc)
+  except requests.exceptions.Timeout as errt:
+    raise AnsibleCMException(message="TimeoutError: cm_api >> " + errt)
+  except requests.exceptions.RequestException as err:
+    raise AnsibleCMException(message="ErrorPath: cm_api >> " + err)
+    
 # Below method is outdated...need to be cleaned up later
 def GETIdByName(name=None, cm_node=None, cm_api_endpoint=None):
     # Create the session object
